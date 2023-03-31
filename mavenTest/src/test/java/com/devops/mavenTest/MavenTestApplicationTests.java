c5c19d3038f13125ba64d876c5a20c3b561537f3
patrarajanikanta <patra.rajanikanta@gmail.com>
2023-03-31 14:18:20 +0530

    Adding Sample Springboot Project

---
+++ mavenTest/src/test/java/com/devops/mavenTest/MavenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.mavenTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
